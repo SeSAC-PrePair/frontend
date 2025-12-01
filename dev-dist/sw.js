698aff0b71830b42bcfe130b29f322e2e44a6335
MoonDongmin <20191836@o365.hanbat.ac.kr>
2025-12-02 02:30:40 +0900

    UI/UX 수정

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -79,7 +79,7 @@ define(['./workbox-b79e8dca'], (function (workbox) { 'use strict';
    */
   workbox.precacheAndRoute([{
     "url": "index.html",
-    "revision": "0.di45i6h19m8"
+    "revision": "0.t53uqrak1c8"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
